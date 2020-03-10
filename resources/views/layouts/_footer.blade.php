453bcede4fa00216a1cfadad02c036ecf44d635a
candy3721 <402404932@qq.com>
2020-03-10 11:31:47 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="/images/pucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
