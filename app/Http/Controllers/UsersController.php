b6cdd94f751d8a8269cbe14ad55ddfdc3d51ea9c
candy3721 <402404932@qq.com>
2020-03-10 16:34:07 +0800

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,11 +19,11 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-    	$this->validate($request,[
-    		'name'=>'required|unique:users|max:50',
-    		'email'=>'required|email|unique:users|max:255',
-    		'password'=>'reuired|confirmed|min:6'
-    	]);
-    	return;
+    	$this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
